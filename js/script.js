26bf9a62a902189248db568e04df5726df14b65b
Naufal Rizky R <nfallrr1001@gmail.com>
2025-04-15 23:01:56 +0700

    Responsive and else

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+let viewer = prompt("Halo, silahkan masukkan nama anda.");
+    console.log(viewer);
+    document.getElementById("viewer-name").innerHTML = viewer;
+
 function validateForm() {
     const name = document.forms["message-form"]["nama"].value;
     const birthDate = document.forms["message-form"]["dob"].value;
@@ -21,6 +25,6 @@ function setSenderUI(name, birthDate, gender, message) {
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-message").innerHTML = message;
     const now = new Date();
-    const formatted = now.toString(); // You can customize this
+    const formatted = now.toString(); //Customizeable
     document.getElementById('current-time').textContent = formatted;
 }
